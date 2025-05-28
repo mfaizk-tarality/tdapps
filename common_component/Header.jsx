f579e02726eb10d8447a33f07eb348783247be43
mfaizk <faiz-react@tarality.com>
2025-05-28 16:31:57 +0530

    token creator completed

--- common_component/Header.jsx
+++ common_component/Header.jsx
@@ -135,6 +135,7 @@ const Header = () => {
             {navItems.map((item) => renderNavItem(item, true))}
           </ul>
         </div>
+        <img src="/assets/brand/logo.svg" alt="" className="h-6 md:h-8" />
         {/* <Link className="btn btn-ghost text-xl"></Link> */}
       </div>
 
