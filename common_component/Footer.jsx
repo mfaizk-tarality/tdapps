99fe12a1d27b6d1452721a58b3b122f536332110
mfaizk <faiz-react@tarality.com>
2025-05-29 11:50:29 +0530

    footer updated

--- common_component/Footer.jsx
+++ common_component/Footer.jsx
@@ -1,37 +1,93 @@
+import {
+  IconBrandFacebook,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
+  const footerSections = [
+    {
+      title: "Services",
+      links: [
+        { name: "Branding", url: "#" },
+        { name: "Design", url: "#" },
+        { name: "Marketing", url: "#" },
+        { name: "Advertisement", url: "#" },
+      ],
+    },
+    {
+      title: "Company",
+      links: [
+        { name: "About us", url: "#" },
+        { name: "Contact", url: "#" },
+        { name: "Jobs", url: "#" },
+        { name: "Press kit", url: "#" },
+      ],
+    },
+    {
+      title: "Legal",
+      links: [
+        { name: "Terms of use", url: "#" },
+        { name: "Privacy policy", url: "#" },
+        { name: "Cookie policy", url: "#" },
+      ],
+    },
+    {
+      title: "Legal",
+      links: [
+        { name: "Terms of use", url: "#" },
+        { name: "Privacy policy", url: "#" },
+        { name: "Cookie policy", url: "#" },
+      ],
+    },
+  ];
+
+  const socials = [
+    {
+      icon: IconBrandFacebook,
+      href: "#",
+    },
+    {
+      icon: IconBrandInstagram,
+      href: "#",
+    },
+    {
+      icon: IconBrandYoutube,
+      href: "#",
+    },
+    {
+      icon: IconBrandLinkedin,
+      href: "#",
+    },
+  ];
+
   return (
     <>
       <div className="flex w-full flex-col">
         <div className="divider"></div>
       </div>
       <footer className="footer sm:footer-horizontal bg-background text-neutral-content p-10 top-div">
-        <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+        {footerSections.map((section, index) => (
+          <nav key={index}>
+            <h6 className="footer-title">{section.title}</h6>
+            {section.links.map((link, linkIndex) => (
+              <a key={linkIndex} className="link link-hover" href={link.url}>
+                {link.name}
+              </a>
+            ))}
+          </nav>
+        ))}
+        <nav className="w-full flex items-end flex-col">
+          {socials?.map((item, idx) => {
+            return (
+              <Link href={item?.href} key={idx}>
+                <item.icon className="cursor-pointer" />
+              </Link>
+            );
+          })}
         </nav>
       </footer>
     </>
